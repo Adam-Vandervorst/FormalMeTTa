38dca21b14f46ca8c49ccb0f05db3c5dcfd0a537
Adam-Vandervorst <adam.vandervorst@gmail.com>
2023-03-15 00:33:35 +0100

    Add Nat example

--- src/main/scala/Terms.scala
+++ src/main/scala/Terms.scala
@@ -4,13 +4,15 @@ import scala.collection.MultiSet
 
 
 case class Space(ts: MultiSet[Term])
-object Space { def apply(ts: Term*) = new Space(MultiSet.from(ts)) } // can't be empty in doc
+object Space:
+  def apply(ts: Term*) = new Space(MultiSet.from(ts)) // can't be empty in doc
 
 sealed trait Term
 
 case class Expr(ts: Vector[Term]) extends Term
 object Expr:
   def apply(t: Term, ts: Term*): Expr = new Expr(t +: ts.toVector)
+  def nest(t: Term, ts: Term*): Term = (t +: ts).reduceRight((x, y) => Expr(Vector(x, y)))
 
 sealed trait Atom extends Term
 
--- src/test/scala/Examples.scala
+++ src/test/scala/Examples.scala
@@ -1,6 +1,31 @@
 import munit.FunSuite
 
 
+class Nats extends FunSuite:
+  test("basic") {
+    val S = StringLiteral("Successor")
+    val Z = StringLiteral("Zero")
+
+    val initial = State(
+      Space(Expr(StringLiteral("Add"), Expr.nest(S, S, S, Z), Expr.nest(S, Z))),
+      Space(
+        Expr(===, Expr(StringLiteral("Add"), Var("xz"), Z), Var("xz")),
+        Expr(===, Expr(StringLiteral("Add"), Var("xs"), Expr(S, Var("ys"))), Expr(StringLiteral("Add"), Expr(S, Var("xs")), Var("ys"))),
+      ),
+      Space(),
+      Space()
+    )
+
+    val resulting = State(
+      Space(),
+      initial.k,
+      Space(),
+      Space(Expr.nest(S, S, S, S, Z))
+    )
+
+     assert(execute(initial, ALL) == resulting)
+  }
+
 
 class FrogUnification extends FunSuite:
   test("basic") {
